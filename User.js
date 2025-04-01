e0b0c43fdf0ab9e57e3aa2ab82ca75543a057d67
vdev080 <80513226+vdev080@users.noreply.github.com>
2025-04-01 11:04:39 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    profileImage: { type: String }, // Store Google profile picture URL
+    firstName: String,
+    lastName: String,
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, sparse: true }, // `sparse: true` allows multiple `null`
+    profileImage: String
 });
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
